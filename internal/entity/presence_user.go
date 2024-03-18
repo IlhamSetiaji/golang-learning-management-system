35bf39fac6228013773d2f53a3248acc9726ec9e
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2024-03-18 21:43:28 +0700

    removed bad syntax on entity

--- internal/entity/presence_user.go
+++ internal/entity/presence_user.go
@@ -7,8 +7,8 @@ type PresenceUser struct {
 	UserID     uint      `gorm:"primaryKey" json:"user_id"`
 	Summary    string    `json:"summary" gorm:"type:text"`
 	Duration   int       `json:"duration"`
-	StartTime  time.Time `json:"start_time" gorm:"type:datetime;`
-	EndTime    time.Time `json:"end_time" gorm:"type:datetime;`
+	StartTime  time.Time `json:"start_time" gorm:"type:datetime;"` // Added closing double quote
+	EndTime    time.Time `json:"end_time" gorm:"type:datetime;"`   // Added closing double quote
 	CreatedAt  time.Time `gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
 	UpdatedAt  time.Time `gorm:"type:datetime;default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
 }
