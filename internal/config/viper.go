bcb38ef2867b80703c24c4243d0027999d3c7b2b
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2024-03-18 14:08:25 +0700

    added viper config

---
+++ internal/config/viper.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+// NewViper is a function to load config from config.json
+// You can change the implementation, for example load from env file, consul, etcd, etc
+func NewViper() *viper.Viper {
+	config := viper.New()
+
+	config.SetConfigName("config")
+	config.SetConfigType("json")
+	config.AddConfigPath("./../")
+	config.AddConfigPath("./")
+	err := config.ReadInConfig()
+
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+
+	return config
+}
