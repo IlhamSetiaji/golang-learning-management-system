7b8adf0e61d3ad9c7d068fc64c1f5690b2078c85
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2024-03-19 12:51:43 +0700

    added database seeder

--- internal/entity/master_class.go
+++ internal/entity/master_class.go
@@ -4,17 +4,24 @@ import "gorm.io/gorm"
 
 type MasterClass struct {
 	gorm.Model
-	BatchID     uint        `json:"batch_id"`
-	Batch       Batch       `gorm:"foreignKey:BatchID;references:ID"`
-	Name        string      `json:"name"`
-	Slug        string      `json:"slug"`
-	Description string      `json:"description"`
-	Image       string      `json:"image"`
-	IsActive    bool        `json:"is_active" gorm:"default:true"`
-	Users       []User      `gorm:"many2many:master_class_users;"`
-	Timelines   []Timeline  `gorm:"foreignKey:MasterClassID;references:ID"`
-	Objectives  []Objective `gorm:"foreignKey:MasterClassID;references:ID"`
-	Presences   []Presence  `gorm:"foreignKey:MasterClassID;references:ID"`
-	Tasks       []Task      `gorm:"foreignKey:MasterClassID;references:ID"`
-	Materials   []Material  `gorm:"foreignKey:MasterClassID;references:ID"`
+	ParentID    uint          `json:"parent_id" gorm:"default:null"` // 0 for no parent
+	Parent      *MasterClass  `gorm:"foreignKey:ParentID;references:ID"`
+	Children    []MasterClass `gorm:"foreignKey:ParentID;references:ID"`
+	BatchID     uint          `json:"batch_id"`
+	Batch       Batch         `gorm:"foreignKey:BatchID;references:ID"`
+	Name        string        `json:"name"`
+	Slug        string        `json:"slug"`
+	Description string        `json:"description"`
+	Image       string        `json:"image" gorm:"type:text;default:null"`
+	EnrollCode  string        `json:"enroll_code" gorm:"unique;default:null"`
+	Price       float64       `json:"price"` // Exported field
+	Duration    int           `json:"duration"`
+	Level       string        `json:"level"`
+	IsActive    bool          `json:"is_active" gorm:"default:true"`
+	Users       []User        `gorm:"many2many:master_class_users;"`
+	Timelines   []Timeline    `gorm:"foreignKey:MasterClassID;references:ID"`
+	Objectives  []Objective   `gorm:"foreignKey:MasterClassID;references:ID"`
+	Presences   []Presence    `gorm:"foreignKey:MasterClassID;references:ID"`
+	Tasks       []Task        `gorm:"foreignKey:MasterClassID;references:ID"`
+	Materials   []Material    `gorm:"foreignKey:MasterClassID;references:ID"`
 }
--- internal/entity/program.go
+++ internal/entity/program.go
@@ -4,16 +4,11 @@ import "gorm.io/gorm"
 
 type Program struct {
 	gorm.Model
-	ParentID          uint            `json:"parent_id" gorm:"default:null"` // 0 for no parent
-	Parent            *Program        `gorm:"foreignKey:ParentID;references:ID"`
 	ProgramCategoryID uint            `json:"program_category_id"`
 	ProgramCategory   ProgramCategory `gorm:"foreignKey:ProgramCategoryID;references:ID"`
 	Name              string          `json:"name"`
 	Description       string          `gorm:"type:text" json:"description"`
-	EnrollCode        string          `json:"enroll_code" gorm:"unique"`
-	Price             float64         `json:"price"` // Exported field
-	Duration          int             `json:"duration"`
-	Level             string          `json:"level"`
+	Image             string          `json:"image" gorm:"type:text;default:null"`
 	IsActive          bool            `json:"is_active" gorm:"default:true"`
 	Batches           []Batch         `gorm:"foreignKey:ProgramID;references:ID"`
 }
