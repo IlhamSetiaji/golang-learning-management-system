5e60fb6d60aa9d973a6c053c15871f19b96e85f5
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2024-03-19 13:22:14 +0700

    added base user use case

---
+++ internal/usecase/user_usecase.go
@@ -0,0 +1,24 @@
+package usecase
+
+import (
+	"github.com/IlhamSetiaji/go-lms/internal/repository"
+	"github.com/go-playground/validator/v10"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+type UserUseCase struct {
+	DB             *gorm.DB
+	Log            *logrus.Logger
+	Validate       *validator.Validate
+	UserRepository *repository.UserRepository
+}
+
+func NewUserUseCase(db *gorm.DB, log *logrus.Logger, validate *validator.Validate, userRepository *repository.UserRepository) *UserUseCase {
+	return &UserUseCase{
+		DB:             db,
+		Log:            log,
+		Validate:       validate,
+		UserRepository: userRepository,
+	}
+}
