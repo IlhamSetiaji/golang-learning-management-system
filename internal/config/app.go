40244805aedae8bc965525a4a367923a1658f359
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2024-03-19 16:31:19 +0700

    added app config

--- internal/config/app.go
+++ internal/config/app.go
@@ -1,1 +1,41 @@
 package config
+
+import (
+	"github.com/IlhamSetiaji/go-lms/internal/http/controller"
+	"github.com/IlhamSetiaji/go-lms/internal/http/route"
+	"github.com/IlhamSetiaji/go-lms/internal/messaging"
+	"github.com/IlhamSetiaji/go-lms/internal/repository"
+	"github.com/IlhamSetiaji/go-lms/internal/usecase"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
+)
+
+type BootstrapConfig struct {
+	DB       *gorm.DB
+	App      *gin.Engine
+	Log      *logrus.Logger
+	Validate *validator.Validate
+	Config   *viper.Viper
+	Producer *messaging.Producer
+}
+
+func Bootstrap(config *BootstrapConfig) {
+	// setup repositories
+	userRepository := repository.NewUserRepository(config.Log)
+
+	// setup usecases
+	userUseCase := usecase.NewUserUseCase(config.DB, config.Log, config.Validate, userRepository, config.Producer)
+
+	// setup controllers
+	userController := controller.NewUserController(config.Log, userUseCase)
+
+	// setup routes
+	routeConfig := route.RouteConfig{
+		App:            config.App,
+		UserController: userController,
+	}
+	routeConfig.SetupRoutes()
+}
--- internal/http/route/route.go
+++ internal/http/route/route.go
@@ -1,1 +1,31 @@
 package route
+
+import (
+	"github.com/IlhamSetiaji/go-lms/internal/http/controller"
+	"github.com/gin-gonic/gin"
+)
+
+type RouteConfig struct {
+	App            *gin.Engine
+	UserController *controller.UserController
+}
+
+func (c *RouteConfig) SetupRoutes() {
+	// user routes
+	c.App.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	c.SetupUserRoutes()
+
+	c.App.Run()
+}
+
+func (c *RouteConfig) SetupUserRoutes() {
+	userRoutes := c.App.Group("/users")
+	{
+		userRoutes.POST("/login", c.UserController.Login)
+	}
+}
